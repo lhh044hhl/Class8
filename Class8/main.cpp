8feac3668edcdd4fecbbc18a6b686dd3bb0415f8
113295634@qq.com <113295634@qq.com>
2020-04-17 16:41:53 +0800

    练习12

--- Class8/main.cpp
+++ Class8/main.cpp
@@ -2,44 +2,7 @@
 using namespace cv;
 using namespace std;
 
-/*int main() {
-	Mat src;
-	Mat src1;
-	Mat dst;
-	Mat statsMat;
-	Mat centroids;
 
-
-
-
-
-	Mat image = imread("E:\\dignal picture\\rim.png", 0);
-	threshold(image, src1, 98, 105, THRESH_BINARY_INV);
-	Mat kernel = getStructuringElement(MORPH_RECT, Size(5, 5));
-	morphologyEx(src1, src,
-		MORPH_OPEN, kernel);
-	int n = connectedComponentsWithStats(src, dst,
-		statsMat, centroids,
-		8, CV_32S
-	);
-	for (int i = 1;i < n;i++) {
-		Rect bndbox;
-		bndbox.x = statsMat.at<int>(i, 0);
-		bndbox.y = statsMat.at<int>(i, 1);
-		bndbox.width = statsMat.at<int>(i, 2);
-		bndbox.height = statsMat.at<int>(i, 3);
-		if (((bndbox.width / bndbox.height) < 1.01) 
-			&& ((bndbox.width / bndbox.height) > 0.99))
-		{
-			rectangle(src, bndbox, CV_RGB(255, 255, 255), 1, 8, 0);
-		}
-	}
-
-	
-	imshow("result", src);
-	cout << n - 1 << endl;
-	waitKey(0);
-}*/
 
 
 //Á·Ï°Ò»
